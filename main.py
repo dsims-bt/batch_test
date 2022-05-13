2a88da716b5aacf73464239e01ab42e27d55145b
Dustin Sims <dustin.sims@belmont.tech>
2022-05-13 15:53:34 -0500

    dont use the same var name

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     daisi = Daisi('Batch Process', base_url='https://dev3.daisi.io')
     result = daisi.give_color(repeat=5)
     for res in result.value:
-        print(result.value)
+        print(res.value)
