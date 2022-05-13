fa3bf6bee43d9f20b6770748302c9fc911138f1f
Dustin Sims <dustin.sims@belmont.tech>
2022-05-13 15:46:50 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from pydaisi import Daisi
+
+if __name__ == '__main__':
+    daisi = Daisi('Batch Process', base_url='https://dev3.daisi.io')
+    result = daisi.give_color(repeat=5)
+    for result in result.value:
+        print(result.value)
